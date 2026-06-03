929e2cc44a4fc6e298cdf90af6cae57d42443e60
Alexandre Marques Rodrigues <sonhosdaily@gmail.com>
2026-06-03 00:23:08 -0300

    Version 1.3
    New interface and style

--- games.js
+++ games.js
@@ -1,252 +1,367 @@
 const gamesDatabase = [
-  // --- JOGOS DO SONIC (ADICIONADOS) ---
   {
     title: "Sonic the Hedgehog (MD)",
+    console: "md",
+    image: "https://static.wikia.nocookie.net/sonic/images/8/87/Sonic_1_2013_icon_05.png/revision/latest?cb=20230412153252",
     page: "sgames.html",
     link: "https://gam.onl/sega-genesis/sonic-the-hedgehog.html",
     search: "sonic sonic 1 md sonic the hedgehog mega drive md genesis sega"
-  },
-  {
+},
+
+{
     title: "Sonic the Hedgehog (MS)",
+    console: "ms",
+    image: "https://cdn2.steamgriddb.com/thumb/d752ec46727553ba73ebef385ab545de.jpg",
     page: "sgames.html",
     link: "https://gam.onl/sega-master-system/sonic-the-hedgehog.html",
     search: "sonic sonic 1 ms sonic the hedgehog master system ms sega"
-  },
-  {
+},
+
+{
     title: "Sonic the Hedgehog (GG)",
+    console: "gg",
+    image: "https://cdn2.steamgriddb.com/thumb/d752ec46727553ba73ebef385ab545de.jpg",
     page: "sgames.html",
     link: "https://gam.onl/sega-gamegear/sonic-the-hedgehog.html",
     search: "sonic sonic 1 gg sonic the hedgehog game gear gg sega"
-  },
-  {
+},
+
+{
     title: "Sonic the Hedgehog 2 (MD)",
+    console: "md",
+    image: "https://cdn2.steamgriddb.com/icon/6b180037abbebea991d8b1232f8a8ca9/32/256x256.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-genesis/sonic-the-hedgehog-2.html",
     search: "sonic sonic 2 md sonic the hedgehog 2 mega drive md genesis sega"
-  },
-  {
+},
+
+{
     title: "Sonic the Hedgehog 2 (MS)",
+    console: "ms",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/40b0de5f68597721b8591fa6701857f7.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-master-system/sonic-the-hedgehog-2.html",
     search: "sonic sonic 2 ms sonic the hedgehog 2 master system ms sega"
-  },
-  {
+},
+
+{
     title: "Sonic the Hedgehog 2 (GG)",
+    console: "gg",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/40b0de5f68597721b8591fa6701857f7.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-gamegear/sonic-the-hedgehog-2.html",
     search: "sonic sonic 2 gg sonic the hedgehog 2 game gear gg sega"
-  },
-  {
+},
+
+{
     title: "Sonic Chaos (MS)",
+    console: "ms",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/9aad055e21af271c91f551b40cd24244.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-master-system/sonic-the-hedgehog-chaos.html",
-    search: "sonic sonic chaos  mschaos master system ms sega"
-  },
-  {
+    search: "sonic sonic chaos mschaos master system ms sega"
+},
+
+{
     title: "Sonic Chaos (GG)",
+    console: "gg",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/9aad055e21af271c91f551b40cd24244.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-gamegear/sonic-chaos.html",
     search: "sonic sonic chaos gg chaos game gear gg sega"
-  },
-  {
+},
+
+{
     title: "Sonic CD (MCD)",
+    console: "mcd",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/ae1eaa32d10b6c886981755d579fb4d8.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-cd/sonic-cd.html",
     search: "sonic sonic cd sega cd mcd classic"
-  },
-  {
+},
+
+{
     title: "Sonic Spinball (MD)",
+    console: "md",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/70e3da32d14d879905434986d67aa833.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-genesis/sonic-the-hedgehog-spinball.html",
     search: "sonic sonic spinball md pinball mega drive md genesis sega"
-  },
-  {
+},
+
+{
     title: "Sonic the Hedgehog 3 (MD)",
+    console: "md",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/ad972f10e0800b49d76fed33a21f6698.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-genesis/sonic-the-hedgehog-3.html",
     search: "sonic sonic 3 sonic the hedgehog 3 mega drive md genesis sega"
-  },
-  {
+},
+
+{
     title: "Sonic Spinball (GG)",
+    console: "gg",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/70e3da32d14d879905434986d67aa833.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-gamegear/sonic-the-hedgehog-spinball.html",
     search: "sonic sonic spinball gg pinball game gear gg sega"
-  },
-  {
+},
+
+{
     title: "Sonic Drift (GG)",
+    console: "gg",
+    image: "https://cdn2.steamgriddb.com/logo_thumb/4d7efe87e47389ee9f84559335b86a63.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-gamegear/sonic-drift.html",
     search: "sonic sonic drift corrida racing kart game gear gg sega"
-  },
-  {
+},
+
+{
     title: "Sonic Triple Trouble (GG)",
+    console: "gg",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/cf5bc9424768595e045fa603f251e534.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-gamegear/sonic-the-hedgehog-triple-trouble.html",
     search: "sonic triple trouble sonic triple trouble game gear gg sega"
-  },
-  {
+},
+
+{
     title: "Sonic & Knuckles (MD)",
+    console: "md",
+    image: "https://cdn2.steamgriddb.com/icon/616fc8538fee3610ac91c81b5741c321/32/256x256.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-genesis/sonic-and-knuckles.html",
     search: "sonic knuckles sonic and knuckles mega drive md genesis sega"
-  },
-  {
+},
+
+{
     title: "Sonic 3 & Knuckles (MD)",
+    console: "md",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/ad972f10e0800b49d76fed33a21f6698.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-genesis/sonic-the-hedgehog-3-and-knuckles.html",
     search: "sonic sonic 3 knuckles sonic 3 and knuckles sonic 3 & knuckles mega drive md genesis sega"
-  },
-  {
+},
+
+{
     title: "Sonic Drift 2 (GG)",
+    console: "gg",
+    image: "https://cdn2.steamgriddb.com/logo_thumb/c2d8230bf04b89e2d4cc27d9c27576f7.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-gamegear/sonic-drift-2.html",
     search: "sonic sonic drift 2 gg corrida racing kart game gear gg sega"
-  },
-  {
+},
+
+{
     title: "Tails' Skypatrol (GG)",
+    console: "gg",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/6a130f1dc6f0c829f874e92e5458dced.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-gamegear/tails-skypatrol.html",
     search: "tails tails skypatrol gg tails sky patrol game gear gg sega"
-  },
-  {
+},
+
+{
     title: "Tails Adventure (GG)",
+    console: "gg",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/5905aa3361a00b7d9356fa6cf222396d.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-gamegear/tails-adventure.html",
     search: "tails tails adventure gg game gear gg sega"
-  },
-  {
+},
+
+{
     title: "Sonic 3D Blast (MD)",
+    console: "md",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/ff451b4a456150649bc16b979c93a383.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-genesis/sonic-3d-blast.html",
     search: "sonic sonic 3d blast flickies mega drive md genesis sega"
-  },
-  {
+},
+
+{
     title: "Sonic Blast (MS)",
+    console: "ms",
+    image: "https://cdn2.steamgriddb.com/thumb/d37f84871a66c3cdc56e168698eb4df3.jpg",
     page: "sgames.html",
     link: "https://gam.onl/sega-master-system/sonic-blast.html",
     search: "sonic sonic blast ms master system ms sega"
-  },
-  {
+},
+
+{
     title: "Sonic Blast (GG)",
+    console: "gg",
+    image: "https://cdn2.steamgriddb.com/thumb/d37f84871a66c3cdc56e168698eb4df3.jpg",
     page: "sgames.html",
     link: "https://gam.onl/sega-gamegear/sonic-blast.html",
     search: "sonic sonic blast gg game gear gg sega"
-  },
-  {
+},
+
+{
     title: "Sonic R (Saturn)",
+    console: "saturn",
+    image: "https://cdn2.steamgriddb.com/icon/082a1d41243d3f37ae65ba1c9d0e2b57/32/256x256.png",
     page: "sgames.html",
     link: "https://www.retrogames.onl/2018/09/sonic-r-saturn.html",
     search: "sonic sonic r ss racing corrida saturn sega"
-  },
-  {
+},
+
+{
     title: "Sonic Advance (GBA)",
+    console: "gba",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/016d35fbe2f0f8985bdbc8e5c7175b68.png",
     page: "sgames.html",
     link: "https://gam.onl/gba/sonic-advance.html",
     search: "sonic sonic advance gba gameboy advance sega"
-  },
-  {
+},
+
+{
     title: "Sonic Advance 2 (GBA)",
+    console: "gba",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/44f95c37a24495521a98b63f0bbf4268.png",
     page: "sgames.html",
     link: "https://gam.onl/gba/sonic-advance-2.html",
     search: "sonic sonic advance 2 gba gameboy advance sega"
-  },
-  {
+},
+
+{
     title: "Sonic Battle (GBA)",
+    console: "gba",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/d14388bb836687ff2b16b7bee6bab182.png",
     page: "sgames.html",
     link: "https://gam.onl/gba/sonic-battle.html",
     search: "sonic sonic battle luta fighting gba gameboy advance sega"
-  },
-  {
+},
+
+{
     title: "Sonic Advance 3 (GBA)",
+    console: "gba",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/5bb5f8d030f5e6e4b6d137c6990f0772.png",
     page: "sgames.html",
     link: "https://gam.onl/gba/sonic-advance-3.html",
     search: "sonic sonic advance 3 gba gameboy advance sega"
-  },
-  {
+},
+
+{
     title: "Sonic Megamix (MD)",
+    console: "md",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/77846f536d53c5f13d9e33a7b511bc9a.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-genesis/sonic-the-hedgehog-megamix.html",
     search: "sonic sonic megamix hack rom mega drive md genesis sega"
-  },
-  {
+},
+
+{
     title: "Sonic Megamix (SCD)",
+    console: "mcd",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/77846f536d53c5f13d9e33a7b511bc9a.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-cd/sonic-the-hedgehog-megamix.html",
     search: "sonic sonic megamix hack rom sega cd scd"
-  },
-  {
+},
+
+{
     title: "Sonic 3 Complete (MD)",
+    console: "md",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/ad972f10e0800b49d76fed33a21f6698.png",
     page: "sgames.html",
     link: "https://gam.onl/sega-genesis/sonic-3-complete.html",
     search: "sonic sonic 3 complete hack rom mega drive md genesis sega"
-  },
-
+},
   // --- OUTROS JOGOS JÁ EXISTENTES ---
-  {
+{
     title: "Resident Evil 1 (PSX)",
-    page: "ogames.html", // Defina a página correta aqui (ex: sgames.html ou ogames.html)
+    console: "psx",
+    page: "ogames.html",
     link: "https://gam.onl/psx/resident-evil.html",
-    search: "resident evil resident evil 1 re re1 psx horror zombie" // Criado com base no padrão do RE2
-  },
-  {
+    image: "https://cdn2.steamgriddb.com/icon/cf2f3fe19ffba462831d7f037a07fc83/32/256x256.png",
+    search: "resident evil resident evil 1 re re1 psx horror zombie"
+},
+
+{
     title: "Resident Evil 2 (PSX)",
+    console: "psx",
     page: "ogames.html",
     link: "https://gam.onl/psx/resident-evil-2.html",
+    image: "https://cdn2.steamgriddb.com/icon/eaa52f3366768bca401dca9ea5b181dd/32/256x256.png",
     search: "resident evil resident evil 2 re2 biohazard psx"
-  },
-  {
+},
+
+{
     title: "Resident Evil 3 (PSX)",
+    console: "psx",
     page: "ogames.html",
     link: "https://gam.onl/psx/resident-evil-3.html",
+    image: "https://cdn2.steamgriddb.com/icon/c57168a952f5d46724cf35dfc3d48a7f/32/256x256.png",
     search: "resident evil resident evil 3 re3 nemesis psx"
-  },
-  {
+},
+
+{
     title: "Crash Team Racing (PSX)",
+    console: "psx",
     page: "ogames.html",
     link: "https://gam.onl/psx/crash-team-racing.html",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/e243aa93e6b6e031797f86d0858f5e40.png",
     search: "crash team racing ctr psx"
-  },
+},
+
 {
     title: "Crash Bandicoot (PSX)",
+    console: "psx",
     page: "ogames.html",
     link: "https://gam.onl/psx/crash-bandicoot.html",
+    image: "https://cdn2.steamgriddb.com/icon/0485d4712950cbce8b00eb3f5111cb36/32/512x512.png",
     search: "crash bandicoot crash 1 psx platform"
 },
 
 {
     title: "Crash Bandicoot 2: Cortex Strikes Back (PSX)",
+    console: "psx",
     page: "ogames.html",
     link: "https://gam.onl/psx/crash-bandicoot-2.html",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/b6e0a198de15835a1c344bf362d978da.png",
     search: "crash bandicoot 2 cortex strikes back crash 2 psx platform"
 },
 
 {
     title: "Crash Bandicoot 3: Warped (PSX)",
+    console: "psx",
     page: "ogames.html",
     link: "https://gam.onl/psx/crash-bandicoot-3.html",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/503d8657d47c066ada31f344b030c352.png",
     search: "crash bandicoot 3 warped crash 3 psx platform"
 },
+
 {
     title: "Castlevania: Symphony of the Night (PSX)",
+    console: "psx",
     page: "ogames.html",
     link: "https://gam.onl/psx/castlevania-symphony-of-the-night.html",
+    image: "https://cdn2.steamgriddb.com/icon_thumb/f9cd8ad72ffc6365652747f613805b1f.png",
     search: "castlevania symphony of the night sotn psx metroidvania dracula alucard"
 },
 
 {
     title: "Castlevania: Aria of Sorrow (GBA)",
+    console: "gba",
     page: "ogames.html",
     link: "https://gam.onl/gba/castlevania-aria-of-sorrow.html",
+    image: "https://cdn2.steamgriddb.com/icon/c8862fc1a32725712838863fb1a260b9/32/256x256.png",
     search: "castlevania aria of sorrow gba gameboy advance metroidvania soma cruz"
 },
+
 {
     title: "Megaman X",
+    console: "snes",
     page: "ogames.html",
     link: "https://gam.onl/snes/mega-man-x.html",
+    image: "https://cdn2.steamgriddb.com/icon/f31a1965338ed4efc2fab8bef7adef4d/32/256x256.png",
     search: "megaman megaman x snes super nintendo entertainment system platformer megaman"
 },
+
 {
     title: "Pokémon Fire Red",
+    console: "gba",
     page: "ogames.html",
     link: "https://gam.onl/gba/pokemon-firered-version-play-online.html",
     image: "https://cdn2.steamgriddb.com/icon_thumb/8103eb779f2cfed843b1b71c6a1b350a.png",
@@ -255,6 +370,7 @@ const gamesDatabase = [
 
 {
     title: "Pokémon Emerald",
+    console: "gba",
     page: "ogames.html",
     link: "https://gam.onl/gba/pokemon-emerald-version-play-online.html",
     image: "https://cdn2.steamgriddb.com/icon_thumb/f6aacf266d86ca416e772e122b784f78.png",
@@ -263,6 +379,7 @@ const gamesDatabase = [
 
 {
     title: "The Legend of Zelda: Ocarina of Time",
+    console: "n64",
     page: "ogames.html",
     link: "https://gam.onl/n64/legend-of-zelda-ocarina-of-time-play-online.html",
     image: "https://cdn2.steamgriddb.com/icon_thumb/3b939e9df1da04be8663c0fee2562c59.png",
@@ -271,6 +388,7 @@ const gamesDatabase = [
 
 {
     title: "Super Mario World",
+    console: "snes",
     page: "ogames.html",
     link: "https://gam.onl/snes/super-mario-world-play-online.html",
     image: "https://cdn2.steamgriddb.com/icon_thumb/8fcbbfa30f0dd6586eae1ad000385394.png",
